c22030b23d0097a498ba83442ccb45af4efced1d
Benyi Cordero <benyi.uriel2006@gmail.com>
2025-05-06 14:33:03 -0600

    Creacion del proyecto

---
+++ pruebaGit/src/main/java/com/pruebaGit/PruebaGitApplication.java
@@ -0,0 +1,14 @@
+package com.pruebaGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.ls.LSOutput;
+
+@SpringBootApplication
+public class PruebaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaGitApplication.class, args);
+	}
+
+}
